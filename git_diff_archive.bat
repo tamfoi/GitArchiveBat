6c9ec5ddd363b90ada987996d9123c0e3f57c0de
tamfoi <tamfoi@users.noreply.github.com>
2018-08-20 20:10:49 +0900

    update

--- git_diff_archive.bat
+++ git_diff_archive.bat
@@ -6,7 +6,6 @@ set BASE_NAME=
 set FORMATTED_DATE=
 set FORMATTED_TIME=
 
-:: COMMIT_ID1がCOMMIT_ID2より新しいコミットという前提
 if "%2" EQU "" (
   set COMMIT_ID1=HEAD
   set COMMIT_ID2=%1
@@ -15,12 +14,8 @@ if "%2" EQU "" (
   set COMMIT_ID2=%2
 )
 
-:: 差分ファイルを抽出、削除ファイルは抽出から除外
-:: `tokens=* delims=`はなくても大丈夫だが一応明示的に指定する
-:: `git diff`がファイル名を改行で区切って出力するため`for in do`で1行ずつ読まれることになるので文字連結する。変数展開の問題で連結処理はサブルーチン化して実現
 for /F "usebackq tokens=* delims=" %%A in (`git diff --diff-filter=ACMRTUXB --name-only %COMMIT_ID2%...%COMMIT_ID1%`) do call :diffListJoin %%A
 
-:: 出力するzipファイル名に必要な情報をセットアップ
 for /F "usebackq tokens=* delims=" %%A in (`git rev-parse --show-toplevel`) do call :setBasename %%A
 set FORMATTED_DATE=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%
 set FORMATTED_TIME=%TIME: =0%
